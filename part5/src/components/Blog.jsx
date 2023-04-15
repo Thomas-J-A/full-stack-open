8f63d40367376aa99cccb92a9d5e4740bb910920
Thomas Aspbury <tomaspbury@icloud.com>
2023-04-15 21:15:45 +0100

    Complete exercise 5.7

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {`${blog.title} | ${blog.author}`}
-  </div>
-);
+import { useState } from 'react';
+
+import Button from './UI/button/Button';
+
+import './blog.css';
+
+const Blog = ({ blog }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpanded = () => setIsExpanded((prev) => !prev);
+
+  return (
+    <div className="blog">
+      <div>
+        {`${blog.title} | ${blog.author}`}
+        <Button text="View" handleClick={toggleExpanded} />
+      </div>
+
+      {isExpanded && (
+        <div>
+          <p>{blog.url}</p>
+          <p>
+            {`Likes: ${blog.likes}`}
+            <Button text="Like" handleClick={null} />
+          </p>
+          <p>{blog.user.name}</p>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
