37fae19ae4b385e46b367620025c6d07d59da465
Thomas Aspbury <tomaspbury@icloud.com>
2023-04-16 20:07:26 +0100

    Update eslint config

--- part5/.eslintrc.cjs
+++ part5/.eslintrc.cjs
@@ -2,19 +2,26 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    'jest/globals': true,
   },
   extends: [
-    'plugin:react/recommended',
     'airbnb',
+    'plugin:react/recommended',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
   ],
   overrides: [
   ],
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
   plugins: [
     'react',
+    'jest',
   ],
   rules: {
     'react/function-component-definition': [2, {
@@ -24,5 +31,10 @@ module.exports = {
     'react/react-in-jsx-scope': 0,
     'react/prop-types': 0,
     'no-console': 0,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 };
